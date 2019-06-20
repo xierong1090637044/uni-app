f0b1e4b129902ce2600d885576df7823539f8a2b
XR1090637044 <1090637044@qq.com>
2019-06-20 19:28:39 +0800

    update

--- pages/mine/setting/setting.vue
+++ pages/mine/setting/setting.vue
@@ -1,6 +1,25 @@
 <template>
 	<view>
-		<view></view>
+		<view class="uni-form-item uni-column">
+			<view class="display_flex item">
+				<view style="margin-right: 10rpx;width: 160rpx;">显示精度</view>
+				<input class="uni-input" type="number" placeholder="有效值0,1,2" @input="set_show_float" />
+			</view>
+
+			<view class="display_flex item">
+				<view style="margin-right: 10rpx;width: 160rpx;">USER账号</view>
+				<input class="uni-input"  placeholder="请输入USER账号" />
+			</view>
+			<view class="display_flex item">
+				<view style="margin-right: 10rpx;width: 160rpx;">UKEY账号</view>
+				<input class="uni-input" placeholder="请输入UKEY账号" />
+			</view>
+			<view class="display_flex item">
+				<view style="margin-right: 10rpx;width: 160rpx;">打印机编号</view>
+				<input class="uni-input" placeholder="请输入打印机编号" />
+			</view>
+
+		</view>
 	</view>
 </template>
 
@@ -8,15 +27,25 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+			set_show_float(e)
+			{
+				console.log(e)
+			}
 		}
 	}
 </script>
 
 <style>
+	.item {
+		background: #fff;
+		padding: 4rpx 8rpx;
+	}
 
+	.uni-input {
+		border-bottom: 1rpx solid#ccc;
+	}
 </style>
