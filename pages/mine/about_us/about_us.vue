fab7e0c18b1a1d2276427a896a47fa606e5cb427
XR1090637044 <1090637044@qq.com>
2019-07-01 20:58:26 +0800

    update

---
+++ pages/mine/about_us/about_us.vue
@@ -0,0 +1,72 @@
+<template>
+	<view>
+		<!--pages/mine/about-us/about-us.wxml-->
+		<text class="desc">
+			「库存表」是一款免费的库存管理小程序，旨在“化繁为简，让库存管理更加便捷，使其成为您的库存小助手”
+		</text>
+		<image :src="imgurl[0]" @tap='handlePreviewImage' mode="widthFix" />
+		<view class="copyright">
+			<view><text>Copyright © 谢荣 All Rights Reserved</text></view>
+			<text>联系方式:微信&QQ➭1090637044</text>
+		</view>
+
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				imgurl:["https://bmob-cdn-23134.bmobcloud.com/2019/07/01/af3304954070ba2c80972f3366498366.jpg"]
+			}
+		},
+		methods: {
+			handlePreviewImage(){
+				uni.previewImage({
+            urls: this.imgurl
+        });
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+/* pages/mine/about-us/about-us.wxss */
+page {
+  display: flex;
+  flex-direction: column;
+	text-align: center
+}
+
+.desc {
+  line-height: 30px;
+  color: #333;
+  text-align: center;
+  display: block;
+  padding: 0 20px;
+  margin-bottom: 50px;
+	margin-top: 20rpx;
+}
+
+image {
+  width: 70%;
+  border-radius: 2px;
+  margin: 0 auto;
+  -moz-box-shadow: 0px 3px 20px #333;
+  -webkit-box-shadow: 0px 3px 20px #333;
+  box-shadow: 0px 3px 20px #333;
+}
+
+.copyright {
+  font-size: 28rpx;
+  color: #999;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 30rpx;
+  text-align: center;
+}
+
+</style>
---
+++ pages/mine/home_page/home_page.vue
@@ -0,0 +1,101 @@
+<template>
+	<view>
+		<view class='frist'>
+			<view class='display_flex_bet item' bindtap='setheaderimg'>
+				<view>头像</view>
+				<view class='display_flex'>
+					<image :src='userInfo.avatarUrl' class='avatar'></image>
+					<fa-icon type="angle-right" size="18" color="#999" style="margin-left: 10rpx;"></fa-icon>
+				</view>
+			</view>
+
+			<view class='display_flex_bet item1'>
+				<view>昵称</view>
+				<view class='display_flex right_item'>
+					<text>{{userInfo.nickname}}</text>
+					<fa-icon type="angle-right" size="18" color="#999" style="margin-left: 10rpx;"></fa-icon>
+				</view>
+			</view>
+
+			<view class='display_flex_bet item1' bindtap='change_sex'>
+				<view>性别</view>
+				<view class='display_flex right_item'>
+					<text v-if="userInfo.sex ==1">男</text>
+					<text v-else>女</text>
+					<fa-icon type="angle-right" size="18" color="#999" style="margin-left: 10rpx;"></fa-icon>
+				</view>
+			</view>
+		</view>
+
+		<view style='margin-top:20rpx' class='frist'>
+			<view class='display_flex_bet item1'>
+				<view>手机号</view>
+				<view class='display_flex right_item'>
+					<text>{{userInfo.mobilePhoneNumber}}</text>
+					<fa-icon type="angle-right" size="18" color="#999" style="margin-left: 10rpx;"></fa-icon>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import faIcon from "@/components/kilvn-fa-icon/fa-icon.vue"
+	export default {
+		components: {
+			faIcon
+		},
+		data() {
+			return {
+				userInfo:uni.getStorageSync("user")
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+.avatar
+{
+  width: 100rpx;
+  height: 100rpx;
+  border-radius: 50%;
+}
+.thumb_background
+{
+  width: 100rpx;
+  height: 100rpx;
+}
+.frist
+{
+  padding: 0 20rpx;
+  background: #fff;
+}
+.item
+{
+  padding: 8rpx 0;
+  border-bottom: 1rpx solid#ddd;
+}
+.item1
+{
+  padding: 20rpx 0;
+  border-bottom: 1rpx solid#ddd;
+}
+.icon_enter
+{
+  margin-left: 20rpx;
+}
+
+.right_item
+{
+  color: #999;
+}
+
+.grjj
+{
+  padding: 20rpx 0rpx;
+  border-bottom: 1rpx solid#ccc;
+}
+</style>
