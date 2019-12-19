28c403cb363e94d3ba0b9d98c51c97bfb5e39e3a
XR1090637044 <1090637044@qq.com>
2019-12-19 17:37:07 +0800

    update

---
+++ utils/notice.js
@@ -0,0 +1,21 @@
+import Bmob from "hydrogen-js-sdk";
+let uid = uni.getStorageSync("uid");
+let that = this;
+export default {
+	//得到消息列表
+	getNoticeList(limitNum) {
+		return new Promise((resolve, reject) => {
+			const query = Bmob.Query("InfoCenter");
+			query.order("-order");
+			const query1 = query.equalTo("toUser", '==', uid);
+			const query2 = query.equalTo("fromUser", '==', "I75puc2O"); //这是系统消息
+			
+			query.or(query1, query2);
+			limitNum?query.limit(limitNum):'';
+			query.find().then(res => {
+				resolve(res)
+			});
+		})
+		
+	},
+}
