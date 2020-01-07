60c3b30342eae703093e5cf9690e10ebc15bf3fd
XR1090637044 <1090637044@qq.com>
2020-01-07 16:50:26 +0800

    update

---
+++ pages/webview/webview.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<web-view :src="url"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url:''
+			}
+		},
+		
+		onLoad() {
+			this.url = uni.getStorageSync("webviewUrl")
+		},
+		
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
