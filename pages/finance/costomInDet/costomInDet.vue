a8d59c4e79eb41f789ae7e29320a1218e56d4b93
XR1090637044 <1090637044@qq.com>
2020-04-10 21:07:52 +0800

    update

--- pages/finance/costomInDet/costomInDet.vue
+++ pages/finance/costomInDet/costomInDet.vue
@@ -1,6 +1,21 @@
 <template>
 	<view>
-
+		<view class="display_flex_bet frist">
+			<view>
+				<view style="font-size: 30rpx;color: #3d3d3d;font-weight: bold;" v-if="custom.custom_name">{{custom.custom_name}}</view>
+				<view>
+					<text>联系电话：</text>
+					<text v-if="custom.custom_phone" style="font-size: 30rpx;color: #3d3d3d;font-weight: bold;">{{custom.custom_phone}}</text>
+					<text v-else style="font-size: 30rpx;color: #3d3d3d;font-weight: bold;">未填写</text>
+				</view>
+			</view>
+			<view class="display_flex">
+				<view class="moneyIcon">待收：￥{{custom.allDebt}}</view>
+				<fa-icon type="angle-right" size="20" color="#ddd"></fa-icon>
+			</view>
+		</view>
+		
+		<view></view>
 	</view>
 </template>
 
@@ -11,8 +26,9 @@
 	export default {
 		data() {
 			return {
-				customId:'',
-				custom:''
+				customId: '',
+				custom: '',
+				shouldGetMoneyList: []
 			}
 		},
 
@@ -28,13 +44,14 @@
 		methods: {
 			//加载数据
 			loadData(type) {
+				let custom
 				uni.showLoading({
 					title: "加载中..."
 				})
 				const query1 = Bmob.Query('customs');
 				query1.get(that.customId).then(res => {
-				  console.log(res)
-					that.custom = res
+					console.log(res)
+					custom = res
 					const query = Bmob.Query("order_opreations");
 					query.equalTo("type", "==", -1);
 					query.equalTo("extra_type", "==", 1);
@@ -42,28 +59,67 @@
 					query.equalTo("custom", "==", that.customId);
 					query.equalTo("debt", ">", 0);
 					query.include("account", "custom");
+					query.limit(500);
 					query.find().then(res => {
+						that.shouldGetMoneyList = res;
 						query.equalTo("type", "==", -1);
 						query.equalTo("extra_type", "==", 1);
 						query.equalTo("master", "==", uid);
 						query.equalTo("custom", "==", that.customId);
 						query.equalTo("debt", ">", 0);
+						query.limit(500);
 						query.include("account", "custom");
 						query.statTo("sum", "debt");
 						query.find().then(res => {
-							console.log(res)
+							
+							custom.allDebt = res[0]._sumDebt
+							that.custom = custom
+							console.log(that.custom.allDebt)
 							uni.hideLoading()
 						})
 						console.log(res)
 					})
 				})
-				
-				
+
+
 			},
 		}
 	}
 </script>
 
 <style>
+	.frist{
+		background: #fff;
+		padding: 20rpx 30rpx;
+	}
+	.moneyIcon {
+		color: #f30;
+		border: 1rpx solid#f30;
+		padding: 0rpx 12rpx;
+		font-weight: bold;
+		text-align: center;
+		font-size: 24rpx;
+		margin-right: 20rpx;
+		border-radius: 4rpx;
+	}
 
+	.uni-common-mt {
+		padding: 30rpx;
+	}
+
+	.content {
+		height: calc(100vh - 80rpx);
+		background: #fff;
+	}
+
+	.info_item {
+		padding: 20rpx 30rpx;
+		background: #fff;
+	}
+
+	.item {
+		border-bottom: 1rpx solid#ccc;
+		padding-bottom: 10rpx;
+		margin-bottom: 10rpx;
+	}
 </style>
